1d40f97e0127c35e11551128ab99432cfbc58b25
ThanishKumar113 <81792530+ThanishKumar113@users.noreply.github.com>
2023-07-18 12:59:56 +0530

    Add files via upload

---
+++ todayDeal.js
@@ -0,0 +1,117 @@
+export const todayDeal = [
+    {
+        img : "https://m.media-amazon.com/images/I/41Diz41FkhL._AC_SY200_.jpg",
+        discount : 52,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41DjFnGQ1FL._AC_SY200_.jpg",
+        discount : 31,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31aNgbvYJKL._AC_SY200_.jpg",
+        discount : 66,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31G1NouVxaL._AC_SY200_.jpg",
+        discount : 80,
+        DealOfDay : "Deal of the Day",
+        
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/31EXkIBVKUL._AC_SY200_.jpg",
+        discount : 50,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/416x-scGWgL._AC_SY200_.jpg",
+        discount : 64,
+        DealOfDay : "Deal of the Day",
+        
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/31VfkewLnlL._AC_SY200_.jpg",
+        discount : 54,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/91kKqnkv9jL._AC_SY200_.jpg",
+        discount : 52,
+        DealOfDay : "Deal of the Day",
+        
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/41fsUIG2fwL._AC_SY200_.jpg",
+        discount : 12,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/317lhW5iHVL._AC_SY200_.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        
+    },
+
+    {
+        img : "https://m.media-amazon.com/images/I/41yxd6A+ZAL._AC_SY200_.jpg",
+        discount : 65,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41wcMkODJLL._AC_SY200_.jpg",
+        discount : 75,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/61QQtY6qtHL._AC_SY200_.jpg",
+        discount : 82,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31aTPRvXiSL._AC_SY200_.jpg",
+        discount : 77,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/21wNUazPKRL._AC_SY200_.jpg",
+        discount : 20,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41bFnhrsyOL._AC_SY200_.jpg",
+        discount : 19,
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/41wyFlObIrL._AC_SY200_.jpg",
+        discount : "25",
+        DealOfDay : "Deal of the Day",
+        
+    },
+    {
+        img : "https://m.media-amazon.com/images/I/31Zq-alRH9L._AC_SY200_.jpg",
+        discount : 32,
+        DealOfDay : "Deal of the Day",
+        
+    },
+
+
+
+]
